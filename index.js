2ccec2da63213a7691df3a56d0efb562d4c5a8a2
Natalia Sorokina <90513456+NataleSv@users.noreply.github.com>
2022-04-09 18:04:46 +0300

    feat: add products arrays

---
+++ index.js
@@ -0,0 +1,79 @@
+const headphones = [
+    {
+        img: "./img/productImg/Apple_BYZ_S8521.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        oldPrice: 3527,
+        rate: 4.7,
+        discount: null
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    },
+    {
+        img: "./img/productImg/Apple_BYZ_S8521.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        oldPrice: null,
+        rate: 4.7,
+        discount: "-20%"
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    }
+]
+
+const wirelessHeadphones = [
+    {
+        img: "./img/productImg/Apple_BYZ_S8521.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        oldPrice: 3527,
+        rate: 4.7,
+        discount: null
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    },
+    {
+        img: "./img/productImg/Apple_EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        oldPrice: null,
+        rate: 4.5,
+        discount:null
+    }
+]
+
+
