1fb254f53a03b8f3a12366397978c7aea3179e14
Natalia Sorokina <90513456+NataleSv@users.noreply.github.com>
2022-04-11 03:19:17 +0300

    feat: create adding and showing products at the cart

--- index.js
+++ index.js
@@ -201,32 +201,32 @@ const cart = {
         title: "Apple BYZ S852I",
         price: 2927,
         oldPrice: 3527,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.7,
         discount: null,
         count: 0,
     },
-    '1':{
+    '1': {
         img: "./img/productImg/Apple_EarPods.png",
         title: "Apple EarPods",
         price: 2327,
         oldPrice: null,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.5,
-        discount:null,
+        discount: null,
         count: 0,
     },
-    '2':{
+    '2': {
         img: "./img/productImg/Apple_EarPods2.png",
         title: "Apple EarPods 2",
         price: 2327,
         oldPrice: null,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.5,
-        discount:null,
+        discount: null,
         count: 0,
     },
-    '3':{
+    '3': {
         img: "./img/productImg/Apple_BYZ_S8521.png",
         title: "Apple BYZ S852I",
         price: null,
@@ -236,24 +236,24 @@ const cart = {
         discount: "-20",
         count: 0,
     },
-    '4':{
+    '4': {
         img: "./img/productImg/Apple_EarPods2.png",
         title: "Apple EarPods 2",
         price: 2327,
         oldPrice: null,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.5,
-        discount:null,
+        discount: null,
         count: 0,
     },
-     '5':{
+    '5': {
         img: "./img/productImg/BOROFONE_BO4.png",
         title: "Apple EarPods",
         price: 2327,
         oldPrice: null,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.5,
-        discount:null,
+        discount: null,
         count: 0,
     },
     '6': {
@@ -261,7 +261,7 @@ const cart = {
         title: "Apple BYZ S852I",
         price: 2927,
         oldPrice: 3527,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.7,
         discount: null,
         count: 0,
@@ -271,9 +271,9 @@ const cart = {
         title: "Apple EarPods",
         price: 2327,
         oldPrice: null,
-        discountPrice:null,
+        discountPrice: null,
         rate: 4.5,
-        discount:null
+        discount: null
     },
     '8': {
         img: "./img/productImg/Apple_EarPods.png",
@@ -282,9 +282,9 @@ const cart = {
         oldPrice: null,
         discountPrice: null,
         rate: 4.5,
-        discount:null
+        discount: null
     },
-    totalCount:0
+    totalCount: 0
 }
 
 
@@ -295,15 +295,14 @@ document.onclick = event => {
     if (event.target.classList.contains('minus')) {
         minusFunction(event.target.dataset.id);
     }
+    if (event.target.classList.contains('deleteProduct')) {
+        deleteFunction(event.target.dataset.id);
+    }
 }
-
-
-
-
-
 
 //увеличение
 const plusFunction = id => {
+
     cart[id]['count']++;
     cart.totalCount++;
     renderCart();
@@ -320,7 +319,7 @@ const minusFunction = id => {
 
 //удаление товара
 const deleteFunction = id => {
-    delete cart[id]['count'];
+    cart[id]['count'] = 0;
     renderCart();
 }
 
@@ -330,17 +329,129 @@ const renderCart = () => {
     console.log(cart);
 }
 
-renderCart();
+document.addEventListener("DOMContentLoaded", addCountCartIcon);
 
+//добавление счетчика на корзину
+function addCountCartIcon (){
+    let productCart = JSON.parse( sessionStorage.cart);
+    if (productCart.totalCount === 0) {
+        countCart.innerHTML = "";
+    } else {
+        countCart.innerHTML = productCart.totalCount;
+    }
+}
 
+document.addEventListener("DOMContentLoaded", createCartProduct);
 
 //вывод карточки
-let productCart = JSON.parse( sessionStorage.cart);
 
 function createCartProduct () {
-    let container = document. getElementsByClassName('containerCart')[0];
-    let basketCard = document.createElement("article");
-    basketCard.className = "card basketCard";
+    let productCart = JSON.parse( sessionStorage.cart);
+   for(let i = 0; i < Object.keys(productCart).length-3; i++){
+       let id = String(i);
+    if (productCart[i]['count'] > 0 ) {
+        let countProdCart = productCart[i]['count'];
+        let price = productCart[i]['price'] || productCart[i]['discountPrice'];
+        let totalProductSum = price * countProdCart;
+        let container = document.getElementsByClassName('containerCart')[0];
+        let basketCard = document.createElement("article");
+        basketCard.className = "card basketCard";
 
+        let mainCardContainer = document.createElement("div");
+        mainCardContainer.className = "mainCardContainer";
+
+        let productInfo = document.createElement("div");
+        productInfo.className = "productInfo";
+
+        let imgCartWrap = document.createElement("div");
+        imgCartWrap.className = "imgCartWrap";
+
+        let imgProduct = document.createElement("img");
+        imgProduct.className = "imgProduct";
+        imgProduct.src = productCart[i]['img'];
+
+        let textWrap = document.createElement("div");
+        textWrap.className = "textWrap";
+
+        let productName = document.createElement("h3");
+        productName.className = "productName";
+        productName.innerHTML = productCart[i]['title'];
+
+        let priceContainer = document.createElement("div");
+        priceContainer.className = "priceContainer";
+
+        let productPrice = document.createElement("p");
+        productPrice.className = "productPrice price";
+        productPrice.innerHTML = productCart[i]['price'] || productCart[i]['discountPrice'];
+
+
+        let deleteProduct = document.createElement("button");
+        deleteProduct.className = "deleteProduct btn";
+
+        let footerCardContainer = document.createElement("div");
+        footerCardContainer.className = "footerCardContainer";
+
+        let countContainer = document.createElement("div");
+        countContainer.className = "countContainer";
+
+        let minusBtn = document.createElement("button");
+        minusBtn.className = "minus countBtn btn";
+        minusBtn.innerHTML = "-";
+        minusBtn.dataset.id = id;
+
+        let countProduct = document.createElement("p");
+        countProduct.className = "count";
+        countProduct.innerHTML = productCart[i]['count'];
+
+        let plusBtn = document.createElement("button");
+        plusBtn.className = "plus countBtn btn";
+        plusBtn.innerHTML = "+";
+        plusBtn.dataset.id = id;
+
+        let finalProductPrice = document.createElement("p");
+        finalProductPrice.className = "finalProductPrice price";
+        finalProductPrice.innerHTML = totalProductSum;
+
+        basketCard.appendChild(mainCardContainer);
+        mainCardContainer.appendChild(productInfo);
+        productInfo.appendChild(imgCartWrap);
+        imgCartWrap.appendChild(imgProduct);
+        productInfo.appendChild(textWrap);
+        textWrap.appendChild(productName);
+        textWrap.appendChild(priceContainer);
+        priceContainer.appendChild(productPrice);
+        mainCardContainer.appendChild(deleteProduct);
+        basketCard.appendChild(footerCardContainer);
+        footerCardContainer.appendChild(countContainer);
+        countContainer.appendChild(minusBtn);
+        countContainer.appendChild(countProduct);
+        countContainer.appendChild(plusBtn);
+        footerCardContainer.appendChild(finalProductPrice);
+        container.appendChild(basketCard);
+
+        addTotalSum ();
+    }
+}
 
 }
+
+function countTotalSum () {
+    let productCart = JSON.parse( sessionStorage.cart);
+    let totalSum=0;
+    for(let i = 0; i < Object.keys(productCart).length-3; i++){
+        if (productCart[i]['count'] > 0 ) {
+            let countProdCart = productCart[i]['count'];
+            let price = productCart[i]['price'] || productCart[i]['discountPrice'];
+            let totalProductSum= price * countProdCart;
+            totalSum+=totalProductSum;
+    }
+    }
+    return totalSum;
+}
+
+function addTotalSum () {
+    let totalSum = countTotalSum();
+    let totalProductsSum = document.getElementsByClassName('totalProductsSum')[0];
+    totalProductsSum.innerHTML = totalSum;
+}
+
